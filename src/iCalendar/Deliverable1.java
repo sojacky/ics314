fa00cfe08090551c60c657b880796a97bf783357
mpsommer <mpsommer@hawaii.edu>
2015-07-09 17:04:08 -1000

    FInished all requirements of deliverable1 specifically Geographic position methods to fill position fields in .ics file and same for the classification fields.  Created "Study for exam" .ics file as specified in the assignment requrements.

--- src/iCalendar/Deliverable1.java
+++ src/iCalendar/Deliverable1.java
@@ -17,14 +17,14 @@ public class Deliverable1 {
 		icsFileFieldsCreator fields = new icsFileFieldsCreator();
 		Scanner scan = new Scanner(System.in);
 
-	
+
 		try {
-			
+
 			//create a new file if it doesn't exist already
-			
-			
+
+
 			System.out.println("Please enter a title for your event");
-			String title = scan.next();
+			String title = scan.nextLine();
 			filePath = title + ".ics";
 			file = new File(filePath);
 			file.createNewFile();
@@ -44,56 +44,81 @@ public class Deliverable1 {
 			bufferedWriter.newLine();
 			bufferedWriter.write(fields.setDateStampString());
 			bufferedWriter.newLine();
-			
+
 			System.out.println("Please enter a start date(YYYY/MM/DD)");
-			String startDate = scan.next();
-			bufferedWriter.write(fields.setStartDateString(startDate));
+			String startDate = scan.nextLine();
+			System.out.println("Please enter a start time(HHMMSS)");
+			String startTime = scan.nextLine();
+			bufferedWriter.write(fields.setStartDateString(startDate, startTime));
 			bufferedWriter.newLine();
-			
+
 			System.out.println("Please enter an end date(YYYY/MM/DD)");
-			String endDate = scan.next();
-			bufferedWriter.write(fields.setEndDateString(endDate));
+			String endDate = scan.nextLine();
+			System.out.println("Please enter a end time(HHMMSS)");
+			String endTime = scan.nextLine();
+			bufferedWriter.write(fields.setEndDateString(endDate, endTime));
 			bufferedWriter.newLine();
-			
+
 
 			bufferedWriter.write(fields.setSummaryString(title));
 			bufferedWriter.newLine();
-			
+
 			System.out.println("Please enter a location");
-			String location = scan.next();
+			String location = scan.nextLine();
 			bufferedWriter.write(fields.setLocationString(location));
-			bufferedWriter.newLine();		
+			bufferedWriter.newLine();	
+
+			System.out.println("Enter a classification if you would like, else type no");
+			String classification = scan.nextLine();
+			if(!classification.equals("no"))
+			{
+				bufferedWriter.write(fields.setClassification(classification));
+				bufferedWriter.newLine();	
+			}
+			
+			System.out.println("Would you like to enter a geographic position? type yes or no");
+			String decision = scan.nextLine();
+			if(decision.equalsIgnoreCase("yes"))
+			{
+				System.out.println("Please enter a latitudinal cooridinate(+/-)00.000000");
+				String latitude = scan.nextLine();
+				System.out.println("Please enter a longitudinal cooridinate(+/-)00.000000");
+				String longitude = scan.nextLine();
+				bufferedWriter.write(fields.setGeographicPosition(latitude, longitude));
+				bufferedWriter.newLine();	
+			}
 			
 			bufferedWriter.write("END:VEVENT");
 			bufferedWriter.newLine();
 			bufferedWriter.write("END:VCALENDAR");
 			
-			
-		
-			 bufferedWriter.close();
-			
+
+
+			bufferedWriter.close();
+			scan.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();	
 		}
-	
 
 
 
-	//		System.out.println("BEGIN:VCALENDAR");
-	//		System.out.println("VERSION:2.0");
-	//		System.out.println("CALSCALE:GREGORIAN");
-	//		System.out.println("BEGIN:VEVENT");
-	//		System.out.println("UID:1234123412341234");
-	//		System.out.println("DTSTAMP:20150708");
-		
-	//		System.out.println("DTSTART;VALUE=DATE:20150801");
-	//		System.out.println("DTEND;VALUE=DATE:20150802");
-		
-		
-	//		System.out.println("SUMMARY:TEST0708");
-	//		System.out.println("END:VEVENT");
-	//		System.out.println("END:VCALENDAR");
 
-}
+		//		System.out.println("BEGIN:VCALENDAR");
+		//		System.out.println("VERSION:2.0");
+		//		System.out.println("CALSCALE:GREGORIAN");
+		//		System.out.println("BEGIN:VEVENT");
+		//		System.out.println("UID:1234123412341234");
+		//		System.out.println("DTSTAMP:20150708");
+
+		//		System.out.println("DTSTART;VALUE=DATE:20150801");
+		//		System.out.println("DTEND;VALUE=DATE:20150802");
+
+
+		//		System.out.println("SUMMARY:TEST0708");
+		//		System.out.println("END:VEVENT");
+		//		System.out.println("END:VCALENDAR");
+
+	}
 
 }
--- src/iCalendar/icsFileFieldsCreator.java
+++ src/iCalendar/icsFileFieldsCreator.java
@@ -8,24 +8,24 @@ import java.util.Date;
 
 public class icsFileFieldsCreator {
 	Calendar cal;
-	
+
 
 	public icsFileFieldsCreator(){};
-	
-	
-	
+
+
+
 	//set the UID 
 	public String setUIDString()
 	{
 		String UID = "";
 		DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-		   cal = Calendar.getInstance();
-		   String date = dateFormat.format(cal.getTime());
-		   UID = "UID:" + date + "@mpsommer";
-		
+		cal = Calendar.getInstance();
+		String date = dateFormat.format(cal.getTime());
+		UID = "UID:" + date + "@mpsommer";
+
 		return UID;
 	}
-	
+
 	//Set the date stamp
 	public String setDateStampString()
 	{
@@ -35,22 +35,21 @@ public class icsFileFieldsCreator {
 		dtStamp = "DTSTAMP:" + dateFormat.format(date);
 		return dtStamp;	
 	}
-	
-	
+
+
 	//Set start date
-	public String setStartDateString(String startDate)
+	public String setStartDateString(String startDate, String startTime)
 	{
 		String dtStart = "";
-
-		dtStart = "DTSTART;VALUE=DATE:" + startDate;
+		dtStart = "DTSTART;TZID=Pacific/Honolulu:" + startDate + "T" + startTime;
 		return dtStart;
 	}
-	
+
 	//Set end date
-	public String setEndDateString(String endDate)
+	public String setEndDateString(String endDate, String endTime)
 	{
 		String dtEnd = "";
-		dtEnd = "DTEND;VALUE=DATE:" + endDate;
+		dtEnd = "DTEND;TZID=Pacific/Honolulu:" + endDate + "T" + endTime;
 		return dtEnd;
 	}
 
@@ -58,62 +57,61 @@ public class icsFileFieldsCreator {
 	public String setSummaryString(String title)
 	{
 		String summary = "";
-		
+
 		summary = "SUMMARY:" + title;
-		
+
 		return summary;
 	}
-	
+
 	//set location
 	public String setLocationString(String location)
 	{
 		return "LOCATION:" + location;
-		
+
 	}
-	
+
 	public String hawaiiStandardTimeCreator()
 	{
 		String timeZoneBlock = 
 				"BEGIN:VTIMEZONE\n"+
-				"TZID:Pacific/Honolulu\n"+
-				"BEGIN:DAYLIGHT\n"+
-				"TZOFFSETFROM:-1030\n"+
-				"DTSTART:19330430T020000\n"+
-				"TZNAME:HDT\n"+
-				"TZOFFSETTO:-0930\n"+
-				"RDATE:19330430T020000\n"+
-				"RDATE:19420209T020000\n"+
-				"END:DAYLIGHT\n"+
-				"BEGIN:STANDARD\n"+
-				"TZOFFSETFROM:-1030\n"+
-				"DTSTART:19470608T020000\n"+
-				"TZNAME:HST\n"+
-				"TZOFFSETTO:-1000\n"+
-				"RDATE:19470608T020000\n"+
-				"END:STANDARD\n"+
-				"END:VTIMEZONE";
-				return timeZoneBlock;
+						"TZID:Pacific/Honolulu\n"+
+						"BEGIN:DAYLIGHT\n"+
+						"TZOFFSETFROM:-1030\n"+
+						"DTSTART:19330430T020000\n"+
+						"TZNAME:HDT\n"+
+						"TZOFFSETTO:-0930\n"+
+						"RDATE:19330430T020000\n"+
+						"RDATE:19420209T020000\n"+
+						"END:DAYLIGHT\n"+
+						"BEGIN:STANDARD\n"+
+						"TZOFFSETFROM:-1030\n"+
+						"DTSTART:19470608T020000\n"+
+						"TZNAME:HST\n"+
+						"TZOFFSETTO:-1000\n"+
+						"RDATE:19470608T020000\n"+
+						"END:STANDARD\n"+
+						"END:VTIMEZONE";
+		return timeZoneBlock;
 	}
-	/*
-	BEGIN:VTIMEZONE
-	TZID:Pacific/Honolulu
-	BEGIN:DAYLIGHT
-	TZOFFSETFROM:-1030
-	DTSTART:19330430T020000
-	TZNAME:HDT
-	TZOFFSETTO:-0930
-	RDATE:19330430T020000
-	RDATE:19420209T020000
-	END:DAYLIGHT
-	BEGIN:STANDARD
-	TZOFFSETFROM:-1030
-	DTSTART:19470608T020000
-	TZNAME:HST
-	TZOFFSETTO:-1000
-	RDATE:19470608T020000
-	END:STANDARD
-	END:VTIMEZONE
 	
-	*/
+	//set classification
+	public String setClassification(String classification)
+	{
+		String result = "ClASS:" + classification;
+		return result;
 	
+	}
+	
+	
+	public String setGeographicPosition(String latitude, String longitude)
+	{
+		return "GEO:" + latitude + ";" + longitude;
+ 	}
+	
+	
+
+
 }
+
+//DTSTART;TZID=Pacific/Honolulu:20150518T150000
+//DTEND;TZID=Pacific/Honolulu:20150518T160000
